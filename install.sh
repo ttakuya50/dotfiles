760dd5fca8b80f1a7c6980ca1787d81621e7370b
TsujiTakuya55 <tsujitakuya50@gmail.com>
2019-11-11 16:16:55 +0900

    refactor:install用のスクリプト修正

--- install.sh
+++ install.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
-DIRECTORY="~/Documents/gitWorkSpace/dotfiles"
-ln -sf ${DIRECTORY}/.bash_profile ~/.bash_profile
-ln -sf ${DIRECTORY}/.zshrc ~/.zshrc
-ln -sf ${DIRECTORY}/.zshenv ~/.zshenv
+DOTPATH=~/.dotfiles
+
+# 使えない場合は curl か wget を使用する
+if [ `which curl` ] || [ `which wget` ]; then
+    tarball="https://github.com/TsujiTakuya55/dotfiles/archive/master.tar.gz"
+
+    # どっちかでダウンロードして，tar に流す
+    #if has "curl"; then
+    if [ `which curl` ]; then
+        curl -L "$tarball"
+
+    #elif has "wget"; then
+    elif [ `which wget` ]; then
+        wget -O - "$tarball"
+
+    fi | tar zxv
+
+    # 解凍したら，DOTPATH に置く
+    mv -f dotfiles-master "$DOTPATH"
+
+else
+    die "curl or wget required"
+fi
+
+cd ~/.dotfiles
+if [ $? -ne 0 ]; then
+    die "not found: $DOTPATH"
+fi
+
+# zshがインストールされていなければインストール実行
+if !(type "zsh" > /dev/null 2>&1); then
+    # 対象のコマンドをインストールするような処理
+    echo "zsh install・・・"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install zsh
+fi
+
+# zplugがインストールされていなければインストール実行
+if !(type "zplug" > /dev/null 2>&1); then
+    # 対象のコマンドをインストールするような処理
+    echo "zplug install・・・"
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+# 移動できたらシンボリックリンクを実行する
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+done
